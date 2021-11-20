d31089e6502c8f9c7f0da8f2749632e4b5822eb9
Artin <artinohanessian@gmail.com>
2021-11-19 21:25:04 -0800

    i dunno need to implement checks :)

---
+++ cmake-build-debug/CMakeFiles/chess_v2.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/chess/Bishop.cpp" "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/cmake-build-debug/CMakeFiles/chess_v2.dir/chess/Bishop.cpp.obj"
+  "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/chess/Chess.cpp" "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/cmake-build-debug/CMakeFiles/chess_v2.dir/chess/Chess.cpp.obj"
+  "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/chess/ComputerPlayer.cpp" "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/cmake-build-debug/CMakeFiles/chess_v2.dir/chess/ComputerPlayer.cpp.obj"
+  "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/chess/HumanPlayer.cpp" "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/cmake-build-debug/CMakeFiles/chess_v2.dir/chess/HumanPlayer.cpp.obj"
+  "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/chess/King.cpp" "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/cmake-build-debug/CMakeFiles/chess_v2.dir/chess/King.cpp.obj"
+  "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/chess/Knight.cpp" "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/cmake-build-debug/CMakeFiles/chess_v2.dir/chess/Knight.cpp.obj"
+  "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/chess/Pawn.cpp" "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/cmake-build-debug/CMakeFiles/chess_v2.dir/chess/Pawn.cpp.obj"
+  "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/chess/Piece.cpp" "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/cmake-build-debug/CMakeFiles/chess_v2.dir/chess/Piece.cpp.obj"
+  "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/chess/Player.cpp" "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/cmake-build-debug/CMakeFiles/chess_v2.dir/chess/Player.cpp.obj"
+  "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/chess/Queen.cpp" "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/cmake-build-debug/CMakeFiles/chess_v2.dir/chess/Queen.cpp.obj"
+  "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/chess/Rook.cpp" "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/cmake-build-debug/CMakeFiles/chess_v2.dir/chess/Rook.cpp.obj"
+  "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/src/AssetManager.cpp" "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/cmake-build-debug/CMakeFiles/chess_v2.dir/src/AssetManager.cpp.obj"
+  "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/src/Game.cpp" "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/cmake-build-debug/CMakeFiles/chess_v2.dir/src/Game.cpp.obj"
+  "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/src/GameState.cpp" "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/cmake-build-debug/CMakeFiles/chess_v2.dir/src/GameState.cpp.obj"
+  "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/src/InputManager.cpp" "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/cmake-build-debug/CMakeFiles/chess_v2.dir/src/InputManager.cpp.obj"
+  "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/src/MainMenuState.cpp" "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/cmake-build-debug/CMakeFiles/chess_v2.dir/src/MainMenuState.cpp.obj"
+  "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/src/StateMachine.cpp" "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/cmake-build-debug/CMakeFiles/chess_v2.dir/src/StateMachine.cpp.obj"
+  "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/src/main.cpp" "C:/Users/Gamer/Desktop/C++ Projects/chess_v2/cmake-build-debug/CMakeFiles/chess_v2.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../headers"
+  "../assets"
+  "../chess"
+  "C:/SFML/SFML-2.5.1/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/chess_v2.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chess_v2.dir/chess/Bishop.cpp.obj"
+  "CMakeFiles/chess_v2.dir/chess/Chess.cpp.obj"
+  "CMakeFiles/chess_v2.dir/chess/ComputerPlayer.cpp.obj"
+  "CMakeFiles/chess_v2.dir/chess/HumanPlayer.cpp.obj"
+  "CMakeFiles/chess_v2.dir/chess/King.cpp.obj"
+  "CMakeFiles/chess_v2.dir/chess/Knight.cpp.obj"
+  "CMakeFiles/chess_v2.dir/chess/Pawn.cpp.obj"
+  "CMakeFiles/chess_v2.dir/chess/Piece.cpp.obj"
+  "CMakeFiles/chess_v2.dir/chess/Player.cpp.obj"
+  "CMakeFiles/chess_v2.dir/chess/Queen.cpp.obj"
+  "CMakeFiles/chess_v2.dir/chess/Rook.cpp.obj"
+  "CMakeFiles/chess_v2.dir/src/AssetManager.cpp.obj"
+  "CMakeFiles/chess_v2.dir/src/Game.cpp.obj"
+  "CMakeFiles/chess_v2.dir/src/GameState.cpp.obj"
+  "CMakeFiles/chess_v2.dir/src/InputManager.cpp.obj"
+  "CMakeFiles/chess_v2.dir/src/MainMenuState.cpp.obj"
+  "CMakeFiles/chess_v2.dir/src/StateMachine.cpp.obj"
+  "CMakeFiles/chess_v2.dir/src/main.cpp.obj"
+  "chess_v2.exe"
+  "chess_v2.exe.manifest"
+  "chess_v2.pdb"
+  "libchess_v2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chess_v2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
